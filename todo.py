fcd91d32aa36e179a8119161e0026a485cb1ed58
Tejaswini-Codes <tejanambula5@gmail.com>
2026-06-23 08:04:16 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,132 +1,110 @@
 from flask import Flask, request, jsonify
-import sqlite3
+import psycopg2
 app = Flask(__name__)
-DATABASE=todo.db
+# Database config
+DB_HOST = "localhost"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASSWORD = "my_password"
 
-#database config
-def get_connection():
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row
-    return conn
-
-#create_table
-def create_table():
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS tasks(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        description TEXT,
-        status TEXT DEFAULT 'Pending',
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+def get_db_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
     )
+
+# CREATE TABLE
+def create_table():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS tasks (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description TEXT,
+            status VARCHAR(20) DEFAULT 'Pending',
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
     """)
-
     conn.commit()
+    cur.close()
     conn.close()
-
-
 create_table()
 
-#home
+# HOME
 @app.route("/")
 def home():
     return jsonify({"message": "To-Do API is Running"})
 
-
-# CREATE TASK
+# ADD TASK
 @app.route("/tasks", methods=["POST"])
 def add_task():
-    data = request.get_json()
-
-    title = data.get("title")
-    description = data.get("description")
-
-    if not title:
-        return jsonify({"error": "Title is required"}), 400
-
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "INSERT INTO tasks(title, description) VALUES(%s, %s)",
+    title = request.json["title"]
+    description = request.json["description"]
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO tasks (title, description) VALUES (%s, %s)",
         (title, description)
     )
-
     conn.commit()
+    cur.close()
     conn.close()
+    return jsonify({"message": "Task added successfully"}), 201
 
-    return jsonify({"message": "Task Added Successfully"}), 201
-
-
-# GET TASKS
+# GET TASK
 @app.route("/tasks", methods=["GET"])
 def get_tasks():
-
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM tasks")
-
-    rows = cursor.fetchall()
-
-    tasks = []
-
-    for row in rows:
-        tasks.append(dict(row))
-
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks")
+    Data = cur.fetchall()
+    cur.close()
     conn.close()
+    tasks = []
+    for row in Data:
+        tasks.append({
+            "id": row[0],
+            "title": row[1],
+            "description": row[2],
+            "status": row[3],
+            "created_at": str(row[4]),
+            "updated_at": str(row[5])
+        })
+    return jsonify(tasks), 200
 
-    return jsonify(tasks)
-
-
-
-
-# UPDATE TASK
+#UPDATE TASK 
 @app.route("/tasks/<int:id>", methods=["PUT"])
 def update_task(id):
-
-    data = request.get_json()
-
-    title = data.get("title")
-    description = data.get("description")
-    status = data.get("status")
-
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("""
+    title = request.json["title"]
+    status = request.json["status"]
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("""
         UPDATE tasks
-        SET title=?,
-            description=?,
-            status=?,
-            updated_at=CURRENT_TIMESTAMP
-        WHERE id=?
-    """, (title, description, status, id))
-
+        SET title = %s,
+            status = %s,
+            updated_at = CURRENT_TIMESTAMP
+        WHERE id = %s
+    """, (title, status, id))
     conn.commit()
+    cur.close()
     conn.close()
+    return jsonify({"message": "Task updated successfully"}), 200
 
-    return jsonify({"message": "Task Updated Successfully"})
-
-
-# DELETE TASK
+#  DELETE TASK
 @app.route("/tasks/<int:id>", methods=["DELETE"])
 def delete_task(id):
-
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("DELETE FROM tasks WHERE id=?", (id,))
-
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM tasks WHERE id = %s", (id,))
     conn.commit()
+    cur.close()
     conn.close()
-
-    return jsonify({"message": "Task Deleted Successfully"})
-
+    return jsonify({"message": "Task deleted successfully"}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
